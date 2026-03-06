90bc6c08de811846744c8d18244c3d5085fed4d3
Mate93934 <mateodjm@gmail.com>
2026-03-06 07:55:04 -0300

    feat(DialogueData): add getters

--- Assets/ScriptableObjects/Dialogues/DialogueData.cs
+++ Assets/ScriptableObjects/Dialogues/DialogueData.cs
@@ -1,8 +1,13 @@
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "DialogueData", menuName = "Dialogue/DialogueData")]
 public class DialogueData : ScriptableObject
 {
     [SerializeField] private string text;
     [SerializeField] private Sprite background;
     [SerializeField] private float duration;
+
+    public string Text => text;
+    public Sprite Background => background;
+    public float Duration => duration;
 }
