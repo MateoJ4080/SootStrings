d7d503cc0819acfa91f8c4cc211661f0bd239470
Mate93934 <mateodjm@gmail.com>
2026-03-06 22:26:40 -0300

    refactor: ShowerEvent to ShowerMission

---
+++ Assets/Scripts/Missions/ShowerMission.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using UnityEngine;
+
+public class ShowerMission : MissionInstance
+{
+    [SerializeField] private DialogueData[] _dialogues;
+    [SerializeField] private AudioClip _showerSound;
+
+    void Start()
+    {
+        // Debug
+        StartCoroutine(Execute());
+        GameEvents.RaiseOnShowerTaken();
+    }
+
+    public override void OnShowerTaken()
+    {
+        StartCoroutine(Execute());
+    }
+
+    private IEnumerator Execute()
+    {
+        DayManager.Instance.SetPlayerActive(false);
+
+        yield return FadeManager.Instance.FadeIn();
+        AudioManager.Instance.PlaySFX(_showerSound);
+        yield return new WaitForSeconds(_showerSound.length);
+        yield return FadeManager.Instance.FadeOut();
+
+        DayManager.Instance.SetPlayerActive(true);
+        StartCoroutine(DialogueManager.Instance.PlaySequence(_dialogues));
+        completed = true;
+    }
+}
