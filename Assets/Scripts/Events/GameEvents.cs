4b457178eabb5f149a42573d014b9224683738e8
Mate93934 <mateodjm@gmail.com>
2026-03-09 10:04:27 -0300

    refactor: GameEvents

--- Assets/Scripts/Events/GameEvents.cs
+++ Assets/Scripts/Events/GameEvents.cs
@@ -3,12 +3,24 @@ using System;
 // Centralized events
 public class GameEvents
 {
+    public static event Action<MissionInstance> MissionCompleted;
+
     public static event Action OnShowerTaken;
     public static event Action OnSlept;
     public static event Action OnPhoneTaken;
 
+    public static void RaiseMissionCompleted(MissionInstance mission)
+    {
+        MissionCompleted?.Invoke(mission);
+    }
+
     public static void RaiseOnShowerTaken()
     {
         OnShowerTaken?.Invoke();
+    }
+
+    public static void RaiseOnSlept()
+    {
+        OnSlept?.Invoke();
     }
 }
